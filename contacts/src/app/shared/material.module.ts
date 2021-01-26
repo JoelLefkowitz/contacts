b010a724cbd5452ec5bc535f32806770ea036c56
Joel <joellefkowitz@hotmail.com>
2021-01-26 13:12:53 +0000

    Initialise angular app

---
+++ contacts/src/app/shared/material.module.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { NgModule } from '@angular/core';
+
+const materialModules = [
+  MatCardModule,
+  MatCardModule,
+  MatDialogModule,
+  MatCheckboxModule,
+  MatButtonModule,
+  MatSelectModule,
+  MatStepperModule,
+  MatIconModule,
+  MatSlideToggleModule,
+  MatPaginatorModule,
+  MatListModule,
+  MatFormFieldModule,
+  MatProgressBarModule, 
+  MatToolbarModule, 
+  MatSidenavModule,
+  MatInputModule, 
+  MatDividerModule
+];
+
+@NgModule({  
+  declarations: [],
+  imports: [
+    CommonModule,    
+  ].concat(materialModules), 
+  exports: [].concat(materialModules)
+})
+export class MaterialModule { }
---
+++ contacts/src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { FabComponent } from './fab/fab.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MaterialModule } from "./material.module";
+import { NgMicroInteractModule } from "ng-micro-interact";
+import { NgModule } from '@angular/core';
+@NgModule({
+  declarations: [FabComponent],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    NgMicroInteractModule
+  ],
+  exports: [MaterialModule, FlexLayoutModule, NgMicroInteractModule]
+})
+export class SharedModule { }
