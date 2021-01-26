294da66b213e4fd2c814f8c7e29363afd881b4c1
Joel <joellefkowitz@hotmail.com>
2021-01-26 12:09:17 +0000

    Add settings files for dev and prod

---
+++ registry/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        install_requires=[
+            "django-debug-toolbar",
+            "django-extensions",
+            "django-silk",
+            "django",
+            "djangorestframework==3.11",
+            "drf-yasg",
+            "randutils",
+            "gunicorn",
+            "psycopg2",
+            "safe_environ",
+            "jsonfield",
+        ],
+        extras_require={
+            "docs": [
+                "sphinx",
+                "pyimport",
+                "pypandoc",
+                "sphinxcontrib.apidoc",
+                "sphinxcontrib.pandoc_markdown",
+                "sphinx-autodoc-annotation",
+                "yummy_sphinx_theme",
+            ],
+            "tests": [
+                "pytest",
+                "pytest-cov",
+                "pytest-html",
+                "pytest-sugar",
+                "pytest-bdd",
+                "pytest-watch",
+            ],
+        },
+    )
